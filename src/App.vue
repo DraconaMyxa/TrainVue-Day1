498fe4eb4b2fad143a131fadeb66ff5b9904ca03
Vaetita Yusabye <vaetita.yusabye@dga.or.th>
2020-04-29 16:24:20 +0700

    Init Project

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TestForm />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TestForm from "./components/TestForm.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    TestForm
   }
-}
+};
 </script>
 
 <style>
